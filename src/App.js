48304120b06f290f997620fd49efd8ae9c03e0ac
Amicht <vicolicht@gmail.com>
2022-06-14 03:23:43 +0300

    home page deleted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import './App.css';
 import Cart from "./components/Cart";
 import ContactUs from "./components/ContactUs";
-import Home from "./components/Home";
 import Store from "./components/Store";
 
 export const CartCtxt = createContext([]);
@@ -16,14 +15,12 @@ function App() {
     <BrowserRouter>
       <div className="container text-center">
         <nav className="nav justify-content-center sticky-top bg-light">
-          <Link className="nav-link" to='/'>דף הבית</Link>
-          <Link className="nav-link" to='/store'>חנות</Link> 
+          <Link className="nav-link" to='/'>חנות</Link> 
           <Link className="nav-link" to='/cart'>העגלה שלי</Link>
           <Link className="nav-link" to='/contact-us'>צור קשר</Link>
         </nav>
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/store" element={<Store />}/>
+          <Route path="/" element={<Store />}/>
           <Route path="/cart" element={<Cart />}/>
           <Route path="/contact-us" element={<ContactUs />}/>
         </Routes>
